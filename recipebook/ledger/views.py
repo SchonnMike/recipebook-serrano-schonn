1ff10cb6cb05d59e253e719c8c7ff86232dc3798
SchonnMike <schonnmike@gmail.com>
2025-02-24 10:42:40 +0800

    updated views.py

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 def home(request):
     return HttpResponse("hello world")
+
+def basicTemplate(request):
+    return render(request, 'basicParams.html')
