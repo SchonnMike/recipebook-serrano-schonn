32353bf13c7e83a67658563b6af345e241c060a6
SchonnMike <schonnmike@gmail.com>
2025-03-04 11:17:49 +0800

    added admin class per models

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 from .models import Recipe, Ingredient, RecipeIngredient
 
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    list_display = ('name',)
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+    list_display = ('name',)
