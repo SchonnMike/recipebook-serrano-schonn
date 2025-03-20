6bf56d8a1099d9b39330e4a2e1ce027af9eb3847
SchonnMike <schonnmike@gmail.com>
2025-03-20 15:34:27 +0800

    added @loginrequired restriction

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
 
+@login_required
 def basicParams(request):
     # recipe list
     recipes = Recipe.objects.all()
