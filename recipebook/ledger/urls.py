7c63c5efc0f863a7aba98c8d58c3555614b67b77
SchonnMike <schonnmike@gmail.com>
2025-04-06 18:12:02 +0800

    added url for adding image

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("recipes/list/", views.basicParams, name="basicParams"),
     path("recipe/<int:num>/", views.tasks, name="tasks"),
     path("recipe/add/", views.add_recipe, name="add_recipe"),
+    path("recipe/<int:pk>/add_image", views.add_image, name="add_image"),
 ]
 
 app_name = "ledger"
