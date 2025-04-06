5115e426d6434ebaeaaf6320f78a237472287f25
SchonnMike <schonnmike@gmail.com>
2025-04-06 17:56:36 +0800

    added url for adding new recipe

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("recipes/list/", views.basicParams, name="basicParams"),
     path("recipe/<int:num>/", views.tasks, name="tasks"),
+    path("recipe/add/", views.add_recipe, name="add_recipe"),
 ]
 
 app_name = "ledger"
