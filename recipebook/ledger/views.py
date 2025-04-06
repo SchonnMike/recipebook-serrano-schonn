0c2977379b1df8e5309ea7cc5aa6ac62c19f197b
SchonnMike <schonnmike@gmail.com>
2025-04-06 22:40:02 +0800

    added RecipeImageForm, fixed structure per form accordingly

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Recipe, RecipeIngredient, RecipeImage
-from .forms import RecipeForm, RecipeIngredientForm, RecipeImageForm
+from .models import Recipe, RecipeIngredient, Ingredient, RecipeImage
+from .forms import RecipeForm, RecipeIngredientForm, IngredientForm, RecipeImageForm
 from django.forms import modelformset_factory
 
 @login_required
@@ -19,33 +19,51 @@ def tasks(request, num):
 
 @login_required
 def add_recipe(request):
-    # handle multiple forms at once
+    # handle multiple forms at once (RecipeForm, RecipeIngredienForm, IngredientForm)
     RecipeIngredientFormSet = modelformset_factory(RecipeIngredient, form=RecipeIngredientForm, extra=3)
 
     if request.method == "POST":
-        form = RecipeForm(request.POST)
-        formset = RecipeIngredientFormSet(request.POST)
 
-        if form.is_valid() and formset.is_valid():
-            recipe = form.save(commit=False)
-            recipe.author = request.user
-            recipe.save()
+        if 'add_recipe' in request.POST:
+            form = RecipeForm(request.POST)
+            if form.is_valid():
+                recipe = form.save(commit=False)
+                recipe.author = request.user
+                recipe.save()
+                return redirect('ledger:tasks', num=recipe.id)
 
-            for ingredient_form in formset:
-                # save each ingredient form
-                ingredient = ingredient_form.save(commit=False)
-                ingredient.recipe = recipe
-                ingredient.save()
-            
-            return redirect('ledger:tasks', num=recipe.id)
+        elif 'add_ingredient' in request.POST:
+            ingredient_form = IngredientForm(request.POST)
+            if ingredient_form.is_valid():
+                ingredient_form.save()
+                return redirect('ledger:add_recipe')
+                
+        elif 'add_recipe_ingredient' in request.POST:
+            formset = RecipeIngredientFormSet(request.POST)
+            if formset.is_valid():
+                for recipe_ingredient_form in formset:
+                    if recipe_ingredient_form.cleaned_data:
+                        # check each ingredient and selected recipe, then link and save
+                        ingredient = recipe_ingredient_form.save(commit=False)
+                        selected_recipe = recipe_ingredient_form.cleaned_data.get('recipe')
+                        if selected_recipe:
+                            ingredient.recipe = selected_recipe
+                        ingredient.save()
+                return redirect('ledger:tasks', num=selected_recipe.id)
         
     else:
         form = RecipeForm()
         formset = RecipeIngredientFormSet(queryset=RecipeIngredient.objects.none())
+        ingredient_form = IngredientForm()
 
     recipes = Recipe.objects.all()
 
-    return render(request, 'add_recipe.html', {'form': form, 'formset': formset, 'recipes': recipes})
+    return render(request, 'add_recipe.html', {
+        'form': form, 
+        'formset': formset, 
+        'ingredient_form': ingredient_form,
+        'recipes': recipes,
+    })
 
 @login_required
 def add_image(request, pk):
@@ -59,4 +77,5 @@ def add_image(request, pk):
             return redirect('ledger:tasks', num=pk)
     else:
         form = RecipeImageForm()
+
     return render(request, 'add_image.html', {'form': form, 'recipe': recipe})
